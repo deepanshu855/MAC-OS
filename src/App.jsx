4a2ce0e5cc67d324d422b7722dca530dcadc01d8
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-05-11 15:27:41 +0530

    MacWindow Wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./app.scss";
 import Dock from "./components/Dock";
 import Navbar from "./components/Navbar";
+import MacWindow from "./components/windows/MacWindow";
 
 const App = () => {
   return <main>
     <Navbar />
+    <MacWindow>
+      Hello
+    </MacWindow>
     <Dock />
   </main>;
 };
